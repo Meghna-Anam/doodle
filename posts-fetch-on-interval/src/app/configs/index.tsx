c82e74d62db403bca4eae1b142cf71db26403501
MyRepo <55554836+Meghna-Anam@users.noreply.github.com>
2021-11-29 08:52:37 +0530

    Delete posts-fetch-on-interval directory

--- posts-fetch-on-interval/src/app/configs/index.tsx
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = process.env.REACT_APP_API_SERVER
-export const FETCH_DATA_INTERVAL_TIME = 10000
--- posts-fetch-on-interval/src/app/utility/enums/http-status-codes.ts
+++
@@ -1,6 +0,0 @@
-export enum HttpStatusCodes {
-  Ok = 200,
-  BadRequest = 400,
-  NotFound = 404,
-  InternalServerError = 500,
-}
